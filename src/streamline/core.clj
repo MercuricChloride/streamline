83a2a7d03f4cc7f50b437d43da62a739cf6cd98f
mercuricchloride <mercuryprotocol@protonmail.com>
2023-12-11 20:22:14 -0800

    Repl server working

--- src/streamline/core.clj
+++ src/streamline/core.clj
@@ -1,19 +1,30 @@
 (ns streamline.core
   (:require
-   [streamline.ast.parser :refer [try-parse]]
-   [streamline.bundle :refer [bundle-file]]
-   [streamline.templating.rust.helpers :refer [use-statements]]
    [compojure.core :refer [defroutes GET]]
    [compojure.route :as route]
+   [nrepl.core :as nrepl]
+   [nrepl.server :refer [start-server]]
    [org.httpkit.server :refer [run-server]])
   (:gen-class))
 
+(def nrepl-port 7869)
+
+(defn test-nrepl
+  []
+  (with-open [conn (nrepl/connect :port nrepl-port)]
+    (-> (nrepl/client conn 1000)
+        (nrepl/message {:op "eval" :code "(+ 1 2 3)"})
+        nrepl/response-values
+        str)))
+
 (defroutes app
   (GET "/" [] "Hello World")
+  (GET "/nrepl" [] (test-nrepl))
   (route/not-found "Not Found"))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (start-server :port nrepl-port)
   (run-server app {:port 8080})
   (println "Running server on port 8080"))
